bbc176d15836d8cf4a5b979281507a178d813166
Gordon DuQuesnay <gduquesnay@gmail.com>
2017-10-02 06:19:32 +0000

    change mail driver

--- bot.rb
+++ bot.rb
@@ -1,28 +1,36 @@
 #!/usr/bin/env ruby
 #
 #
-DISTRIBUTION_LIST = ENV['DISTRIBUTION_LIST'] || 'thomas.howe@tendigittext.com'
+DISTRIBUTION_LIST = ENV['DISTRIBUTION_LIST'] || 'robertjd333@gmail.com'
 API_KEY= ENV['API_KEY'] || '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'
 
 require "./lib/greenbot.rb"
 require 'mail'
 
 Mail.defaults do
-  delivery_method :smtp,  :address    => "smtp.sendgrid.net",
-                          :port       => 25,
-                          :user_name  => 'apikey',
-                          :password   => API_KEY,
-                          :enable_ssl => true
+ # delivery_method :smtp, {   :address    => "smtp.sendgrid.net",
+ #                         :port       => 25,
+ #                         :user_name  => 'apikey',
+ #                         :password   => API_KEY,
+ #                         :authentication => 'plain',
+ #                         :enable_starttls_auto => true }
+  delivery_method :smtp, { :address   => "smtp.sendgrid.net",                                                     
+      :port      => 25,                                                                     
+      :user_name => "apikey",                                                                
+      :password  => API_KEY, 
+      :authentication => 'plain',                                                            
+      :enable_starttls_auto => true }
 end
 
 extraInfo = gets
 bodyText = "A new session has started" + extraInfo
 
-mail = Mail.new do
+Mail.deliver do
   to   DISTRIBUTION_LIST
+  subject "Hello Friend"
   from 'noreply@tendigittext.com'
   body bodyText
 end
 
-mail.deliver!
-
+puts "Email sent"
+puts DISTRIBUTION_LIST
