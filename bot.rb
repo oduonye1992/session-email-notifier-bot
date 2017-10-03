5612cfae6160812dafacc1ad5a34ad103a93876d
Daniel Oduonye <daniel@Macs-MacBook-Pro.local>
2017-10-03 15:22:31 +0100

    Add configurable email subject and message

--- bot.rb
+++ bot.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby
-#
-#
-DISTRIBUTION_LIST = ENV['DISTRIBUTION_LIST'] || 'robertjd333@gmail.com'
+
+DISTRIBUTION_LIST = ENV['DISTRIBUTION_LIST']
 API_KEY= ENV['API_KEY']
 MAIL_ADDRESS= ENV['MAIL_ADDRESS'] 
-MAIL_PORT= ENV['MAIL_PORT'] 
+MAIL_PORT= ENV['MAIL_PORT']
 MAIL_PASSWORD= ENV['MAIL_PASSWORD'] 
 MAIL_USER_NAME= ENV['MAIL_USER_NAME'] 
+MAIL_SUBJECT= ENV['MAIL_SUBJECT']
+MAIL_BODY= ENV['MAIL_BODY']
 
 require "./lib/greenbot.rb"
 require 'mail'
@@ -21,14 +22,13 @@ Mail.defaults do
 end
 
 extraInfo = gets
-bodyText = "A new session has started" + extraInfo
+bodyText = MAIL_BODY + " " + extraInfo
 
 Mail.deliver do
   to   DISTRIBUTION_LIST
-  subject "Hello Friend"
+  subject MAIL_SUBJECT
   from 'noreply@tendigittext.com'
   body bodyText
 end
 
 puts "Email sent"
-puts DISTRIBUTION_LIST
